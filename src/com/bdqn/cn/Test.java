6fadcb4ad5db23d31bc7bd44c48f495bdc9e4fd9
myeclipse_user <myeclipse@bdnq.com>
2020-11-17 14:56:40 +0800

    vfrege

--- src/com/bdqn/cn/Test.java
+++ src/com/bdqn/cn/Test.java
@@ -2,6 +2,8 @@ package com.bdqn.cn;
 
 public class Test {
 	 public static void main(String[] args) {
-			System.out.println("这是git");	
+			System.out.println("这是git");
+			System.out.println("");
+			
 			}
 }
