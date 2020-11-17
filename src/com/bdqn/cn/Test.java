f50230b3dbbcdfb0f98cbce138098d9bb1b2792c
myeclipse_user <myeclipse@bdnq.com>
2020-11-17 15:59:17 +0800

    范围广

--- src/com/bdqn/cn/Test.java
+++ src/com/bdqn/cn/Test.java
@@ -3,7 +3,7 @@ package com.bdqn.cn;
 public class Test {
 	 public static void main(String[] args) {
 			System.out.println("这是git");
-			System.out.println("");
+			System.out.println("反反复复");
 			
 			}
 }
