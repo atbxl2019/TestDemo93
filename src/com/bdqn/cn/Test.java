86596113d41f18eb8474defc5163786b99081dc5
myeclipse_user <myeclipse@bdnq.com>
2020-11-18 14:39:00 +0800

    2020-11-18提交Test文件

--- src/com/bdqn/cn/Test.java
+++ src/com/bdqn/cn/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	 public static void main(String[] args) {
 			System.out.println("这是git");
 			System.out.println("反反复c d复");
-			System.out.println("cccccc");
+			System.out.println("ccccccddcdscsdcddd");
 		}
 }
