21b2173911aa9601cdf31e4a0a0d4271728008a2
myeclipse_user <myeclipse@bdnq.com>
2020-11-18 14:35:56 +0800

    cds

--- src/com/bdqn/cn/Test.java
+++ src/com/bdqn/cn/Test.java
@@ -3,7 +3,7 @@ package com.bdqn.cn;
 public class Test {
 	 public static void main(String[] args) {
 			System.out.println("这是git");
-			System.out.println("反反复复");
+			System.out.println("反反复c d复");
 			System.out.println("cccccc");
 		}
 }
